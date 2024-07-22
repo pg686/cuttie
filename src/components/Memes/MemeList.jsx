e9ba3555f7fafd69fe69394b7170ef74841982b6
pg686 <83918472+pg686@users.noreply.github.com>
2024-07-22 21:42:16 +0300

    update memes

--- src/components/Memes/MemeList.jsx
+++ src/components/Memes/MemeList.jsx
@@ -15,17 +15,17 @@ const MemeList = () => {
       </motion.h2>
       <Memes
         memesArr={[
-          { data: "1", type: "img" },
+          { data: "animation10", type: "gif" },
           { data: "2", type: "img" },
           { data: "3", type: "img" },
           { data: "animation8", type: "gif" },
           { data: "animation9", type: "gif" },
-          { data: "animation10", type: "gif" },
+          { data: "1", type: "img" },
           { data: "animation11", type: "gif" },
           { data: "animation12", type: "gif" },
           { data: "animation13", type: "gif" },
           { data: "4", type: "img" },
-          { data: "animation5", type: "gif" },
+          { data: "11", type: "img" },
           { data: "5", type: "img" },
           { data: "animation4", type: "gif" },
           { data: "5", type: "img" },
@@ -37,7 +37,8 @@ const MemeList = () => {
           { data: "animation7", type: "gif" },
           { data: "9", type: "img" },
           { data: "10", type: "img" },
-          { data: "11", type: "img" },
+          { data: "animation5", type: "gif" },
+         
           { data: "12", type: "img" },
           { data: "13", type: "img" },
           { data: "14", type: "img" },
